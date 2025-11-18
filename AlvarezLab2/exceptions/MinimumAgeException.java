298c25d242864436843bf78ecfd2486a2aea0e2e
Jorgeandres Alvarez <96158104+Jalvarez0311@users.noreply.github.com>
2025-11-17 21:48:49 -0500

    added rest of exception files for the folder

---
+++ AlvarezLab2/exceptions/MinimumAgeException.java
@@ -0,0 +1,8 @@
+package Alvarez_Lab2.exceptions;
+//todo comment
+public class MinimumAgeException extends Exception{
+    
+    public MinimumAgeException(String message){
+        super(message);
+    }
+}
---
+++ AlvarezLab2/exceptions/MissingCandidatesException.java
@@ -0,0 +1,8 @@
+package Alvarez_Lab2.exceptions;
+//todo comment
+public class MissingCandidatesException extends Exception{
+    
+    public MissingCandidatesException(String message){
+        super(message);
+    }
+}
