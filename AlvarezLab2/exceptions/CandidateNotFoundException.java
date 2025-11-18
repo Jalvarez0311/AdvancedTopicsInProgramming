21962dae5b67cfac183719d6397eae6363199190
Jorgeandres Alvarez <96158104+Jalvarez0311@users.noreply.github.com>
2025-11-17 21:47:43 -0500

    Add CandidateNotFoundException class

---
+++ AlvarezLab2/exceptions/CandidateNotFoundException.java
@@ -0,0 +1,7 @@
+package Alvarez_Lab2.exceptions;
+
+public class CandidateNotFoundException extends Exception {
+    public CandidateNotFoundException(String message){
+         super(message);
+    }
+}
