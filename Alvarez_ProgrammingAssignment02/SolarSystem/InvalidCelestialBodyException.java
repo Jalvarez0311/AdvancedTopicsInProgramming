c55d34c87c2252c90e07980331cc107a03326471
Jorgeandres Alvarez <96158104+Jalvarez0311@users.noreply.github.com>
2025-11-19 15:47:03 -0500

    Add files via upload

---
+++ Alvarez_ProgrammingAssignment02/SolarSystem/InvalidCelestialBodyException.java
@@ -0,0 +1,7 @@
+package Alvarez_ProgrammingAssignment02.Exceptions;
+
+public class InvalidCelestialBodyException extends Exception{
+    public InvalidCelestialBodyException (String message){
+        super(message);
+    }
+}
