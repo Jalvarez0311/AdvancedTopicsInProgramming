2e0a4fbab4756b2a9fcc79d723d9076886b5e9ad
Jorgeandres Alvarez <96158104+Jalvarez0311@users.noreply.github.com>
2025-11-25 11:59:33 -0500

    Add InvalidCelestialBodyException class

---
+++ Alvarez_ProgrammingAssignment03/exceptions/InvalidCelestialBodyException.java
@@ -0,0 +1,7 @@
+package Alvarez_ProgrammingAssignment03.exceptions;
+
+public class InvalidCelestialBodyException extends Exception{
+    public InvalidCelestialBodyException(String message){
+        super(message);
+    }
+}
