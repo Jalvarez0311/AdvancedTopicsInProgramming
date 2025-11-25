2d0ceb2b265a1dfcf10c6055519e3e2cfc9e0381
Jorgeandres Alvarez <96158104+Jalvarez0311@users.noreply.github.com>
2025-11-25 12:01:26 -0500

    Add files via upload

---
+++ Alvarez_ProgrammingAssignment03/exceptions/DuplicateCelestialBodyException.java
@@ -0,0 +1,7 @@
+package Alvarez_ProgrammingAssignment03.exceptions;
+
+public class DuplicateCelestialBodyException extends Exception{
+    public DuplicateCelestialBodyException(String message){
+        super(message);
+    }
+}
